b0046a449eeabb07f8a5d6bb50496418d4a051ae
CritterCodes <jacobaengel55@gmail.com>
2024-09-28 21:33:49 -0500

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { db } from './lib/database.lib.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -9,8 +14,19 @@ app.use(cors({
   origin: '*'
 }));
 
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-import { db } from './lib/database.js';
+client.commands = new Collection();
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+
 
 // TODO: Environment based configs
 const config = {
@@ -20,7 +36,7 @@ const config = {
   maxPoolSize: 10,
 };
 
-db.init(config);
+// db.init(config);
     
 // Middleware here
 // Routes here
@@ -29,5 +45,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// Log in to Discord with your client's token
+client.login(process.env.TOKEN);
 
 export default app;
